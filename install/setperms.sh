dcef434617f57e40e7c78ebab52d79f81c958d03
Florian Knodt <git@adlerweb.info>
2014-06-10 23:02:36 +0200

    Create temp folder

--- install/setperms.sh
+++ install/setperms.sh
@@ -3,4 +3,6 @@ chmod 0644 *
 chmod 0755 ./esniperstart.sh
 chmod 0755 ./updateDB.php
 chmod 0755 ./reload.php
+mkdir tmp
+chmod 0777 tmp
 echo "Do not forget to check the interpreter path in updateDB.php, reload.php and esniperstart.sh"
